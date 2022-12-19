569dd96bcc58886a8ce5fd8fc3c1a1ba22ee8509
Arnaud BODEL <contact@osw3.net>
2022-12-19 11:43:54 +0100

    -

--- service-http/src/app/app.module.ts
+++ service-http/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+
+    BooksModule,
+    AuthorsModule,
+
     AppRoutingModule
   ],
   providers: [],
