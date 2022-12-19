4c50808eed6023d6c5ce8dd08853776a3a4d702e
Arnaud BODEL <contact@osw3.net>
2022-12-19 11:46:53 +0100

    -

--- service-http/src/app/home/home-routing.module.ts
+++ service-http/src/app/home/home-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = []
+const routes: Routes = [{
+    path: 'accueil',
+    component: HomeComponent
+}];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
